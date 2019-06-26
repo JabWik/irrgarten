037cba364cd8489bac584f8e5b0122b5650e0d74
Wiktoria Jabłońska <30338072+JabWik@users.noreply.github.com>
2019-06-26 14:02:35 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+ male or female?" << endl;
+    cin >> sex;
+/*
+    cin >> menu;
+
+    switch(menu)
+    {
+    case 1:
+        {
+            cout << "\n\nCHOOSE YOUR CHARACTER\n\n1.Warrior\n2.Archer\n3.Mage";
+            break;
+        }
+    case 2:
+        {
+            cout << "The Author is a cake~";
+            break;
+        }
+    case 3:
+        {
+            cout << "EXIT";
+            break;
+        }
+    }
+
+
+*/
+
+
+
+
+
+
+    return 0;
+}
